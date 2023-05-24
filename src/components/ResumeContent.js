79b8c5e0d60cf4fc5d8cc382c154cd2615bc4466
Jyotsna Jha <jyotsna.jha003@gmail.com>
2023-05-24 18:43:01 +0545

    commit3

--- src/components/ResumeContent.js
+++ src/components/ResumeContent.js
@@ -10,7 +10,10 @@ const ResumeContent = () => {
         <div className="resume-summary">
           <h2 className="section-title">Resume Summary</h2>
           <p className="summary-text">
-            Experienced advocate with a strong background in handling legal cases and providing legal advice. Skilled in client representation, legal research, and dispute resolution. Committed to upholding justice and advocating for clients' rights. 
+            Experienced advocate with a strong background in handling legal
+            cases and providing legal advice. Skilled in client representation,
+            legal research, and dispute resolution. Committed to upholding
+            justice and advocating for clients' rights.
           </p>
         </div>
 
@@ -19,12 +22,14 @@ const ResumeContent = () => {
           <div className="experience-item">
             <h3 className="experience-title">Advocate</h3>
             <p className="experience-details">
-             Sajha Law Firm  | 2023 - Present
+              Sajha Law Firm | 2023 - Present
             </p>
             <ul className="experience-description">
               <li>Represented clients in various civil and criminal cases.</li>
               <li>Conducted legal research and prepared case strategies.</li>
-              <li>Negotiated settlements and facilitated dispute resolution.</li>
+              <li>
+                Negotiated settlements and facilitated dispute resolution.
+              </li>
               <li>Drafted legal documents and contracts.</li>
             </ul>
           </div>
@@ -41,14 +46,11 @@ const ResumeContent = () => {
             <li>Case management</li>
           </ul>
         </div>
-
         <div className="resume-education">
           <h2 className="section-title">Education</h2>
           <div className="education-item">
             <h3 className="education-degree">Bachelor in Law (B.L)</h3>
-            <p className="education-details">
-              Nepal Law Campus | 2015 - 2018
-            </p>
+            <p className="education-details">Nepal Law Campus | 2015 - 2018</p>
           </div>
         </div>
       </div>
